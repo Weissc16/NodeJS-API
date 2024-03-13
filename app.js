155f723df7d98b9c1ae2071319ab632a88bc8cc8
Cody Weiss <weissc16@gmail.com>
2024-03-13 13:58:34 -0600

    using use() and send() functions.

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const http = require(`http`);
-
 const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>The "Add Product" page.</h1>');
+});
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
