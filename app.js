3c123909dc11c46d73f432f4fe37a9c8aaaa7907
Cody Weiss <weissc16@gmail.com>
2024-03-21 10:09:31 -0600

    CSS, Views, Util
    Added multiple folders that hold different code.
    Views holding our HTML code
    Public holding our CSS code
    util holding our Path code
    routes holding our router code.

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>The "Add Product" page.</h1>');
-});
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
 
-app.use('/', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) =>{
+    res.status(404).sendFile(path.join(__dirname, 'views', 'PageNotFound.html'));
+}); 
+
+
 
 app.listen(3000);
