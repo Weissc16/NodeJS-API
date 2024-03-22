5203ec14186b45245e6ec62a0770951a40acbe8c
Cody Weiss <weissc16@gmail.com>
2024-03-22 11:40:17 -0600

    Sharing Data Across requests * Users
    utilized an array variable to store data when passed from the browser.

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const path = require('path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin.js');
+const adminData = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) =>{
-    res.status(404).sendFile(path.join(__dirname, 'views', 'PageNotFound.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 }); 
 
 
