373c25d2848010269df6cf5d711030c96826267c
Cody Weiss <weissc16@gmail.com>
2024-04-06 10:35:48 -0600

    creating a user model
    created an association
      belongsTo()
      hasMany()
    
    created a manual user.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 
 const errorController = require('./controllers/error.js');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,15 +20,40 @@ const shopRoutes = require('./routes/shop.js');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404Page);
 
-sequelize.sync().then(result => {
-    // console.log(result);
-    app.listen(3000);
-})
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+sequelize
+    // .sync({ force: true })
+    .sync()
+    .then(result => {
+        return User.findByPk(1);
+        // console.log(result);
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({ id: 1, name: 'Max', email: 'test@test.com' });
+        }
+        return user;
+    })
+    .then(user => {
+        console.log(user);
+        app.listen(3000);
+    })
     .catch(err => {
         console.log(err);
     });
