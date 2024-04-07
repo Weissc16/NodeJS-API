311cffab641c7e92fc0e925eb1aad57d7705f1d5
Cody Weiss <weissc16@gmail.com>
2024-04-06 23:17:31 -0600

    Cart through Sequelize
    fix the cart using Sequelize.
    added relation between cart, products and cartitems.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const errorController = require('./controllers/error.js');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -36,6 +38,10 @@ app.use(errorController.get404Page);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
     // .sync({ force: true })
@@ -51,7 +57,10 @@ sequelize
         return user;
     })
     .then(user => {
-        console.log(user);
+        // console.log(user)
+        return user.createCart();
+    })
+    .then(cart => {
         app.listen(3000);
     })
     .catch(err => {
